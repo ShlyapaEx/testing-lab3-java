c720eafdb0bdb184c51ea324865e2f4eeab359e5
ShlyapaEx <musha742@gmail.com>
2022-10-22 23:34:50 +0300

    ebook final changes

--- src/EBook/EBook.java
+++ src/EBook/EBook.java
@@ -14,11 +14,22 @@ public class EBook extends Book {
             String file_format, Integer file_size) {
         super(author_surname, author_firstname, title, release_year, sold_count);
 
+        // Формат файла электронной книги
         format_ok(file_format.trim());
         this.file_format = file_format.trim();
 
+        // Размер файла электронной книги
         file_size_ok(file_size);
         this.file_size = file_size;
+    }
+
+    public Integer getFile_size() {
+        return this.file_size;
+    }
+
+    public Integer setFile_size(Integer file_size) {
+        file_size_ok(file_size);
+        return this.file_size = file_size;
     }
 
     public String getFile_format() {
@@ -45,6 +56,12 @@ public class EBook extends Book {
     @Override
     public String toString() {
         return "{" +
+                " author_surname='" + getAuthor_surname() + "'" +
+                ", author_firstname='" + getAuthor_firstname() + "'" +
+                ", title='" + getTitle() + "'" +
+                ", release_year='" + getRelease_year() + "'" +
+                ", sold_count='" + getSold_count() + "'" +
+                ", co_authors='" + getCo_authors() + "'" +
                 " file_format='" + getFile_format() + "'" +
                 "}";
     }
